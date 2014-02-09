158b9de7f761d9ef77e5e7f272ff35a1b4632497
root <root@gateway.home.rackhax.com>
2014-02-09 15:19:06 -0500

    modied file name

--- lib/config.rb
+++ lib/config.rb
@@ -2,7 +2,7 @@ require 'require.rb'
 require 'yaml'
 
 class Rconfig
-	config = YAML.load_file("config.yml")
+	config = YAML.load_file("rvnstat.yml")
     class << self; attr_accessor :vnstat_path end
     class << self; attr_accessor :interface end
     class << self; attr_accessor :port	end
