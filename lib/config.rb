3a7fe2918949ce788ac8d34881ff23fced58c269
root <root@gateway.home.rackhax.com>
2014-02-09 13:38:44 -0500

    added new config variables

--- lib/config.rb
+++ lib/config.rb
@@ -5,7 +5,9 @@ class Rconfig
 	config = YAML.load_file("config.yml")
     class << self; attr_accessor :vnstat_path end
     class << self; attr_accessor :interface end
+    class << self; attr_accessor :port	end
     
     @vnstat_path = config["vnstat_path"]
     @interface = config["interface"]
+    @port = config["port"]
   end
